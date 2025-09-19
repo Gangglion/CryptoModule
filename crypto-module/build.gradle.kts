6e60358e29a4707fa217832d9382d6058959aafb
SeungHyeob Han <o0tmdguq0o@gmail.com>
2025-09-19 17:46:14 +0900

    update : 모듈 build.gradle 에서 참조하는 라이브러리 alias 사용 해제
    - 모듈의 유연성을 위해 버전을 직접 명시해주는 방식으로 변경(다른 프로젝트에 적용되었을때 alias 가 꼬일 수 있음)

--- crypto-module/build.gradle.kts
+++ crypto-module/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -33,10 +33,10 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.12.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
